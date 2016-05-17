8f09166749492b75bdb68055ad37acc54acee4fb
anbarasiu <anbarasiu@gmail.com>
2016-05-17 22:34:46 +0530

    Waterbom

--- src/client/app/album.jsx
+++ src/client/app/album.jsx
@@ -1,6 +1,7 @@
 /*
 * Component to communicate with the Pinterest API and display the images
 * References : http://stackoverflow.com/questions/27731857/get-domain-name-for-pinterest-api
+*              https://api.pinterest.com/v3/search/pins/?join=via_pinner,board,pinner&page_size=50&query=travel&access_token=MTQzMTU5NDozNDYwMDMzMjEyODAwNTg4OTQ6OTIyMzM3MjAzNjg1NDc3NTgwN3wxNDYyNjQ4NDk1OjI1OTIwMDAtLWE2YzgyMWQyOTQ2MjViODk5ZjI5YTE3Y2U4MTBiYjM4
 * TODO: Git Ignore to Access Token file
 */
 
@@ -8,29 +9,29 @@ import React from 'react'
 import {render} from 'react-dom';
 import $ from 'superagent';
 
+import CONSTANTS from './constants.jsx';
 import AlbumItem from './albumItem.jsx';
 
 var Album = React.createClass ({
 
   getInitialState(){
     return {
-      album : [],
-      accessToken : ''
+      album : []
     };
   },
 
   componentDidMount(){
     let params = {
       join : 'via_pinner, board, pinner',
-      page_size : 50,
+      page_size : 100,
       query : 'travel',
-      access_token : this.state.accessToken
+      access_token : CONSTANTS.ACCESS_TOKEN
     };
     let request = $.get('https://api.pinterest.com/v3/search/pins/', params)
                     .end((error, response) => {
                     if(response && response.status === 200){
                       let responseText = JSON.parse(response.text);
-                      this.setState({album : responseText.terms});
+                      this.setState({album : responseText.data});
                       console.log('Results acquired!', this.state.album);
                     }
                     else
@@ -39,9 +40,12 @@ var Album = React.createClass ({
   },
 
   render(){
-  var items = this.state.album.map(function(curr, i){
-    return <AlbumItem item={curr}> </AlbumItem>
-  });
+    var items = this.state.album.map(function(curr, i){
+      return <AlbumItem item={curr}> </AlbumItem>
+    }).sort(function(a,b){
+        return b.props.item.repin_count - a.props.item.repin_count;
+      });
+
     return (
       <section id = "album">
         {items}
--- src/client/app/albumItem.jsx
+++ src/client/app/albumItem.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
-class AlbumItem extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-  }
+var AlbumItem = React.createClass({
+  getInitialState(){
+    return {
+      item : this.props.item
+    };
+  },
 
   render(){
+  var item = this.state.item;
+  console.log(item.repin_count);
     return (
-      <span> Album Item </span>
+    <div className = "card">
+      <h3> {item.title} </h3>
+      <a href = {item.link}>
+        <img src = {item.image_medium_url} width = {item.image_medium_size_pixels.width} height = {item.image_medium_size_pixels.height} />
+      </a>
+      <div>{item.description}</div>
+    </div>
     )
   }
-}
+});
 
 export default AlbumItem;
